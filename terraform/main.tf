f5e9adf5adbb64cfd7a7817ee933adda5d982f96
yogeshb1407 <yogeshbadgujar1407@gmail.com>
2025-02-04 15:37:33 +0530

    terraforme

--- terraform/main.tf
+++ terraform/main.tf
@@ -244,6 +244,10 @@ resource "aws_instance" "ubuntu_instance" {
     Name = "gab-ec2-${terraform.workspace}"
   }
 
+
+
+
   # security group with this instance
   vpc_security_group_ids = [aws_security_group.eks_node_sg.id]
 }
+// the code is ok
